1d1ac25aa35d1826976a1a7b750c0e64249b0e30
anuragsangal1993 <111427780+anuragsangal1993@users.noreply.github.com>
2022-10-02 12:26:09 +0530

    JAVA extraction shell code

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -27,7 +27,110 @@ aws dynamodb scan --table-name UserPosts --max-items 1 --starting-token eyJFeGNs
 aws dynamodb scan --table-name UserPosts --max-items 1 --starting-token eyJFeGNsdXNpdmVTdGFydEtleSI6IG51bGwsICJib3RvX3RydW5jYXRlX2Ftb3VudCI6IDJ9
 
 # Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --tabl#!/bin/bash
+
+# Demo Projection Expression
+aws dynamodb scan --table-name UserPosts --projection-expression "user_id, content"
+
+# Demo Filter Expression
+aws dynamodb scan --table-name UserPosts --filter-expression "user_id = :u" --expression-attribute-values '{ ":u": {"S":"john123"}}'
+
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+aws dynamodb scan --table-name UserPosts --max-items 1 --starting-token eyJFeGNsdXNpdmVTdGFydEtleSI6IG51bGwsICJib3RvX3RydW5jYXRlX2Ftb3VudCI6IDF9
+
+# Fetch the next item
+aws dynamodb scan --table-name UserPosts --max-items 1 --starting-token eyJFeGNsdXNpdmVTdGFydEtleSI6IG51bGwsICJib3RvX3RydW5jYXRlX2Ftb3VudCI6IDJ9
+
+# Page Size demo: will do 1 API call if you have 3 Items
 aws dynamodb scan --tabl
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb sca
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next itemn --table-name UserPosts
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next iteme-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+# Page Size demo: will do 1 API call if you have 3 Items
+aws dynamodb scan --table-name UserPosts 
+
+# Will do 3 API calls if you have 3 Items
+aws dynamodb scan --table-name UserPosts --page-size 1
+
+# Max Item demo:
+aws dynamodb scan --table-name UserPosts --max-items 1
+
+# Fetch the next item
+
 # Page Size demo: will do 1 API c#!/bin/bash
 
 # Demo Projection Expression
